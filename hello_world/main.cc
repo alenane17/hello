084b5148ebd68dfe0d32a22fbe37f25a27efd99c
nito <nito@Class-03.kl9.uspu.ru>
2020-09-15 17:11:32 +0500

    Добавили условие

--- hello_world/main.cc
+++ hello_world/main.cc
@@ -5,10 +5,26 @@
  *      Author: nito
  */
 #include <iostream>
+#include <cmath>
+
+constexpr double Pi = acos(-1.);
 
 int main()
 {
 	std::cout << "Привет, мир" << std::endl;
+	std::cout << "А сейчас  будем находить длину окружности и площадь круга" << std::endl;
+
+	std::cout << "Введите радиус окружности: ";
+	double radius;
+	std::cin >> radius;
+
+	double
+	length = 2 * Pi * radius,
+	area = Pi * radius * radius;
+
+	std:: cout<<
+			"Длина окружности = " << length << std::endl <<
+			"Площадь круга = " << area << std::endl;
 
 	return 0;
 }
