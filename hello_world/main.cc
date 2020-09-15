358110bc31df47c310403c6a8dee6b8d58ea5221
nito <nito@Class-03.kl9.uspu.ru>
2020-09-15 18:18:06 +0500

    Небольшое хулиганство с ситуацией

--- hello_world/main.cc
+++ hello_world/main.cc
@@ -8,28 +8,58 @@
 #include <cmath>
 #include <iomanip>
 
-constexpr double Pi = acos(-1.);
+//constexpr double Pi = acos(-1.);
 
 int main()
 {
-	std::cout << "Привет, мир" << std::endl;
-	std::cout << "А сейчас  будем находить длину окружности и площадь круга" << std::endl;
+//	std::cout << "Привет, мир" << std::endl;
+//	std::cout << "А сейчас  будем находить длину окружности и площадь круга" << std::endl;
+//
+//	std::cout << "Введите радиус окружности: ";
+//	double radius;
+//	std::cin >> radius;
+//
+//	double
+//	length = 2 * Pi * radius,
+//	area = Pi * radius * radius;
+//
+//	std:: cout<<
+//			std::fixed <<
+//			"Длина окружности = " <<
+//			std::setw(9) << std::setprecision(3) << length << std:: endl <<
+//			"Площадь круга    = " <<
+//			std::setw(9) << std::setprecision(3) <<area << std::endl;
 
-	std::cout << "Введите радиус окружности: ";
-	double radius;
-	std::cin >> radius;
+	std::cout <<"Щас будем табулировать синус!" << std::endl;
 
-	double
-	length = 2 * Pi * radius,
-	area = Pi * radius * radius;
+	double start, end, step;
+	std::cout  << "Введите начало промежутка: ";
+	std::cin >> start;
+	std::cout  << "Введите конец промежутка: ";
+	std::cin >> end;
+	std::cout  << "Введите шаг: ";
+	std::cin >> step;
 
-	std:: cout<<
-			std::fixed <<
-			"Длина окружности = " <<
-			std::setw(9) << std::setprecision(3) << length << std:: endl <<
-			"Площадь круга = " <<
-			std::setw(9) << std::setprecision(3) <<area << std::endl;
+	std::cout <<                                 //Выводит шапки таблицы
+			"       x   |  sin(x) \n"
+			"  .....................\n";
 
+	std::cout << std::fixed;                       //Выводим числа с фикс.точкой
+
+	for (double x = start; x<= end; x+=step) {
+		double y = sin(x);
+		int w = int (2 + (y+1.)*20. );
+		std::cout <<
+				std::setw(9) <<
+				std::setprecision(3) <<
+				x << "  |  " <<
+				std::setw(8) <<
+				std::setprecision(5) <<
+				y <<
+				std::setw(w) << '*' <<
+				//sin(x) <<
+				std::endl;
+	}
 	return 0;
 }
 
